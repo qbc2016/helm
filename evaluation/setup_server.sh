f0c527d3490e150125672457c904d5d51496049f
qbc <qianbingchen.qbc@alibaba-inc.com>
2023-06-28 20:22:14 +0800

    Update setup_server.sh

--- evaluation/setup_server.sh
+++ evaluation/setup_server.sh
@@ -3,6 +3,16 @@
 set -e
  
 SUITE_NAME=test
+PORT=8000
+
+while getopts "n:p:" flag
+do
+    # shellcheck disable=SC2220
+    case "${flag}" in
+        n) SUITE_NAME=${OPTARG};;
+        p) PORT=${OPTARG};;
+    esac
+done
 
 PATH_HELM=/root/src/crfm-helm
 PATH_WORKDIR=/root/src/crfm-helm
@@ -15,4 +25,4 @@ export PYTHONPATH=$PYTHONPATH:${PATH_HELM}/src/
 /root/miniconda3/bin/python ${PATH_HELM}/src/helm/benchmark/presentation/summarize.py \
         --suite ${SUITE_NAME}
 
-/root/miniconda3/bin/python ${PATH_HELM}/src/helm/benchmark/server.py -o ${PATH_WORKDIR}/benchmark_output -p 8088
+/root/miniconda3/bin/python ${PATH_HELM}/src/helm/benchmark/server.py -o ${PATH_WORKDIR}/benchmark_output -p ${PORT}
